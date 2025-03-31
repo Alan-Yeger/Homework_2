f567a027396b2b8219ab89959f679cba39d4c051
root <root@Notebook-Alan>
2025-03-31 13:30:36 -0300

    Progress on ex1

---
+++ ej_1/Time.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+#include "Time.hpp"
+
+using namespace std;
+
+Time::Time() {
+    this->hh = 0;
+    this->mm = 0;
+    this->ss = 0;
+    this->isDay = 0;
+}
+Time::Time(int hh) {
+    this->hh = hh;
+    this->mm = 0;
+    this->ss = 0;
+    this->isDay = 0;
+}
+Time::Time(int hh, int mm) {
+    this->hh = hh;
+    this->mm = mm;
+    this->ss = 0;
+    this->isDay = 0;
+}
+Time::Time(int hh, int mm, int ss) {
+    this->hh = hh;
+    this->mm = mm;
+    this->ss = ss;
+    this->isDay = 0;
+}
+Time::Time(int hh, int mm, int ss, bool isDay) {
+    this->hh = hh;
+    this->mm = mm;
+    this->ss = ss;
+    this->isDay = isDay;
+}
+void Time::setHour(int hh) {
+    this->hh = hh;
+}
+void Time::setMinute(int mm) {
+    this->mm = mm;
+}
+void Time::setSecond(int ss) {
+    this->ss = ss;
+}
+void Time::setAmPm(bool isDay) {
+    this->isDay = isDay;
+}
+void Time::getHour() {
+    cout << "La hora es: " << this->hh << "h\n";
+}
+void Time::getMinute() {
+    cout << "Los minutos son: " << this->mm << "h\n";
+}
+void Time::getSecond() {
+    cout << "Los segundos son: " << this->ss << "h\n";
+}
+void Time::getAmPm() {
+    cout << (this->isDay ? "La hora es pm\n" : "La hora es am\n");
+}
+void Time::getTime() {
+    cout << this->hh * this->isDay << ":" << this->mm << ":" << this->ss << "\n";
+}
---
+++ ej_1/Time.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+class Time {
+    public:
+        Time();
+        Time(int hh);
+        Time(int hh, int mm);
+        Time(int hh, int mm, int ss);
+        Time(int hh, int mm, int ss, bool isDay);
+        int hh;
+        int mm;
+        int ss;
+        bool isDay;
+    private:
+        void setHour(int hh);
+        void setMinute(int mm);
+        void setSecond(int ss);
+        void setAmPm(bool isDay);
+        void getHour();
+        void getMinute();
+        void getSecond();
+        void getAmPm();
+        void getTime();
+};
